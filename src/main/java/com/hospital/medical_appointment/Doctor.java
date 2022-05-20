f9849564248e17969dbe51140f8fb25d1c3c2a28
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-20 14:54:29 -0500

    Doctor entity finished

--- src/main/java/com/hospital/medical_appointment/Doctor.java
+++ src/main/java/com/hospital/medical_appointment/Doctor.java
@@ -1,4 +1,39 @@
 package com.hospital.medical_appointment;
 
-public class Doctor {
+import co.com.sofka.domain.generic.Entity;
+import com.hospital.medical_appointment.values.DoctorId;
+import com.hospital.medical_appointment.values.Name;
+import com.hospital.medical_appointment.values.Email;
+
+import java.util.Objects;
+
+public class Doctor extends Entity<DoctorId> {
+
+    private Name name;
+    private Email email;
+
+    public Doctor(DoctorId entityId, Name name, Email email) {
+        super(entityId);
+        this.name = name;
+        this.email = email;
+    }
+
+    // Behaviors
+    public void updateName(Name name){
+        this.name = Objects.requireNonNull(name);
+    }
+
+    public void updateEmail(Email email){
+        this.email = Objects.requireNonNull(email);
+    }
+
+    // Getters
+    public Name name() {
+        return name;
+    }
+
+    public Email email() {
+        return email;
+    }
+
 }
