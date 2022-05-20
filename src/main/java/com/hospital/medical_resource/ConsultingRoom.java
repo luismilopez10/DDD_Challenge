1566108ece4cb5f3f7848f27e2d7c6a39540e71c
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-20 17:30:23 -0500

    'ConsultingRoom' entity finished

--- src/main/java/com/hospital/medical_resource/ConsultingRoom.java
+++ src/main/java/com/hospital/medical_resource/ConsultingRoom.java
@@ -1,4 +1,27 @@
 package com.hospital.medical_resource;
 
-public class ConsultingRoom {
+import co.com.sofka.domain.generic.Entity;
+import com.hospital.medical_resource.values.Address;
+import com.hospital.medical_resource.values.ConsultingRoomId;
+
+import java.util.Objects;
+
+public class ConsultingRoom extends Entity<ConsultingRoomId> {
+
+    private Address address;
+
+    public ConsultingRoom(ConsultingRoomId entityId, Address address) {
+        super(entityId);
+        this.address = address;
+    }
+
+    // Getters
+    public Address address() {
+        return address;
+    }
+
+    // Behaviors
+    public void updateAddress(Address address){
+        this.address = Objects.requireNonNull(address);
+    }
 }
