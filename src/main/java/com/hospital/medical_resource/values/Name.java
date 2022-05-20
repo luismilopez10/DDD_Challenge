f66677fe2a74c7de83e3c9ab43fc7b798c7edfb8
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-20 15:21:27 -0500

    Value object 'Name' added to entity 'MedicalResource'

---
+++ src/main/java/com/hospital/medical_resource/values/Name.java
@@ -0,0 +1,10 @@
+package com.hospital.medical_resource.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class Name implements ValueObject<String> {
+    @Override
+    public String value() {
+        return null;
+    }
+}
