0329a92941438454b4d9487d378de3d3c8d5b4a7
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-20 17:41:54 -0500

    'Phonendoscope' entity finished

--- src/main/java/com/hospital/medical_resource/Phonendoscope.java
+++ src/main/java/com/hospital/medical_resource/Phonendoscope.java
@@ -1,4 +1,27 @@
 package com.hospital.medical_resource;
 
-public class Phonendoscope {
+import co.com.sofka.domain.generic.Entity;
+import com.hospital.medical_resource.values.Brand;
+import com.hospital.medical_resource.values.PhonendoscopeId;
+
+import java.util.Objects;
+
+public class Phonendoscope extends Entity<PhonendoscopeId> {
+
+    private Brand brand;
+
+    public Phonendoscope(PhonendoscopeId entityId, Brand brand) {
+        super(entityId);
+        this.brand = brand;
+    }
+
+    // Getters
+    public Brand brand() {
+        return brand;
+    }
+
+    // Behaviors
+    public void updateBrand(Brand brand) {
+        this.brand = Objects.requireNonNull(brand);
+    }
 }
