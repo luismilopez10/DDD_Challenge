72b6d4a7ee1cc83af3084dddc421452207a64316
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-21 19:29:19 -0500

    Create aggregate commands added

--- src/main/java/com/hospital/medical_resource/commands/CreateMedicalResource.java
+++ src/main/java/com/hospital/medical_resource/commands/CreateMedicalResource.java
@@ -1,6 +1,23 @@
 package com.hospital.medical_resource.commands;
 
 import co.com.sofka.domain.generic.Command;
+import com.hospital.medical_appointment.values.Name;
+import com.hospital.medical_resource.values.MedicalResourceId;
 
 public class CreateMedicalResource extends Command {
+    private MedicalResourceId medicalResourceId;
+    private Name name;
+
+    public CreateMedicalResource(MedicalResourceId entityId, Name name) {
+        medicalResourceId = entityId;
+        this.name = name;
+    }
+
+    public MedicalResourceId getMedicalResourceId() {
+        return medicalResourceId;
+    }
+
+    public Name getName() {
+        return name;
+    }
 }
