624c893a3eac09dda1b5fa7a9387e67f36b1ed0f
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-22 01:57:28 -0500

    UseCases and Tests added

---
+++ src/main/java/com/hospital/usecase/medical_appointment/UpdateClinicHistoryDiagnosisUseCase.java
@@ -0,0 +1,23 @@
+package com.hospital.usecase.medical_appointment;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import com.hospital.medical_appointment.MedicalAppointment;
+import com.hospital.medical_appointment.commands.UpdateClinicHistoryDiagnosis;
+
+public class UpdateClinicHistoryDiagnosisUseCase  extends UseCase<RequestCommand<UpdateClinicHistoryDiagnosis>, ResponseEvents> {
+    @Override
+    public void executeUseCase(RequestCommand<UpdateClinicHistoryDiagnosis> updateClinicHistoryDiagnosisRequestCommand) {
+        var command = updateClinicHistoryDiagnosisRequestCommand.getCommand();
+        var medicalAppointment = MedicalAppointment.from(
+                command.getMedicalAppointmentId(),
+                repository().getEventsBy(command.getMedicalAppointmentId().value()));
+
+        medicalAppointment.UpdateClinicHistoryDiagnosis(
+                command.getEntityId(),
+                command.getDiagnosis());
+
+        emit().onResponse(new ResponseEvents(medicalAppointment.getUncommittedChanges()));
+    }
+}
